0198edd4c2bfd3a83d545630bbe96c09b7f200e6
Fujiwara0805 <tiki4091@gmail.com>
2022-11-02 09:51:40 +0900

    コード修正①

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord 
   belongs_to :user
   has_many :answers
-  has_many :likes
+  has_one_attached :image
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 end
